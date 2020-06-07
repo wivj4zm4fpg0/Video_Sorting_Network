d6e050da8b4ac048bb9d68e48ccc2e0b44795f95
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-08 00:36:27 +0900

    update

--- CNN_LSTM_Model.py
+++ CNN_LSTM_Model.py
@@ -28,19 +28,19 @@ class CNN_LSTM(nn.Module):
         # batch_first = False
         batch_first = True
         if bidirectional:
-            # self.lstm = nn.LSTM(resnet18_last_dim, int(lstm_dim / 2), bidirectional=True, num_layers=2,
+            self.lstm = nn.LSTM(resnet18_last_dim, int(lstm_dim / 2), bidirectional=True, num_layers=2,
+                                batch_first=batch_first)
+            # self.lstm1 = nn.LSTM(resnet18_last_dim, 1024, bidirectional=True, num_layers=2,
             #                     batch_first=batch_first)
-            self.lstm1 = nn.LSTM(resnet18_last_dim, 1024, bidirectional=True, num_layers=2,
-                                batch_first=batch_first)
-            self.lstm2 = nn.LSTM(2048, 2048, bidirectional=True, num_layers=2,
-                                batch_first=batch_first)
+            # self.lstm2 = nn.LSTM(2048, 2048, bidirectional=True, num_layers=2,
+            #                     batch_first=batch_first)
             # self.lstm = nn.LSTM(4096, int(lstm_dim / 2), bidirectional=True, num_layers=2,
             #                     batch_first=batch_first)
         else:
             self.lstm = nn.LSTM(resnet18_last_dim, lstm_dim, bidirectional=False, num_layers=2, batch_first=batch_first)
 
-        # self.fc = nn.Linear(lstm_dim, class_num)
-        self.fc = nn.Linear(4096, class_num)
+        self.fc = nn.Linear(lstm_dim, class_num)
+        # self.fc = nn.Linear(4096, class_num)
         nn.init.kaiming_normal_(self.fc.weight)
 
     # xの形は(バッチサイズ, RNNへの入力数, チャンネル数, 解像度, 解像度)の5次元配列である必要がある
@@ -63,7 +63,8 @@ class CNN_LSTM(nn.Module):
         # x = x.permute(1, 0, 2)  # (batch_size, seq_len, data_size) -> (seq_len, batch_size, data_size)
 
         # x = x.permute(1, 0, 2, 3, 4)  # (batch_size, seq_len, img) -> (seq_len, batch_size, img)
-        x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(sequence_length)])
+        # x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(sequence_length)])
+        x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(batch_size)])
         # (batch_size, seq_len, data_size)
         # output_shape -> (seq_len, batch_size, data_size)
 
@@ -76,9 +77,9 @@ class CNN_LSTM(nn.Module):
         #     fs[i, :, :] = cnn
         # x = fs
 
-        # x = self.lstm(x)[0]
-        x = self.lstm1(x)[0]
-        x = self.lstm2(x)[0]
+        x = self.lstm(x)[0]
+        # x = self.lstm1(x)[0]
+        # x = self.lstm2(x)[0]
         x = self.fc(x)
         return x
 
