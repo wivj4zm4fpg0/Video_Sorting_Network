3adcfc7f2ee72377de6fe9752d32fe22f3c58b94
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-07 18:36:10 +0900

    update

--- video_sort_train_loader.py
+++ video_sort_train_loader.py
@@ -61,7 +61,7 @@ class VideoSortTrainDataSet(VideoTrainDataSet):  # video_train_loader.VideoTrain
         # RandomCropの設定を行う. 引数に画像サイズが必要なので最初のフレームを渡す
         self.pre_processing.transforms[0].set_param(Image.open(frame_list[0]))
         # RandomHorizontalFlipのソースコード参照．pの値を設定．0なら反転しない，1なら反転する
-        self.pre_processing.transforms[1].p = randint(0, 1)
+        self.pre_processing.transforms[1].p = random.randint(0, 1)
 
         pre_processing = lambda image_path: self.pre_processing(Image.open(image_path).convert('RGB'))
         # リスト内包表記で検索
