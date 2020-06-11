1acc1f4e3dfa0939f1d1606818c27eb49e8a948b
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-12 00:21:08 +0900

    update

--- OPN.py
+++ OPN.py
@@ -1,23 +1,15 @@
+import itertools
+import math
+
 import torch
 from torch import nn
 from torchvision.models import resnet18
 from torchvision.models.resnet import BasicBlock
 
 
-class CNN_LSTM(nn.Module):
-    def __init__(self, class_num: int = 101, bidirectional: bool = True, pretrained: bool = True,
-                 task: str = 'classification'):
+class OPN(nn.Module):
+    def __init__(self, pretrained: bool = True, frame_num=4):
         super().__init__()
-
-        assert task == 'classification' or task == 'sorting'
-        if task == 'classification':
-            self.forward_method = self.classification_forward
-            # self.forward = self.classification_forward
-            batch_first = False
-        elif task == 'sorting':
-            self.forward_method = self.sorting_forward
-            # self.forward = self.sorting_forward
-            batch_first = True
 
         resnet18_modules = [module for module in (resnet18(pretrained=pretrained).modules())][1:-1]
         resnet18_modules_cut = resnet18_modules[0:4]
@@ -27,63 +19,22 @@ class CNN_LSTM(nn.Module):
         self.resnet18 = nn.Sequential(*resnet18_modules_cut)
         resnet18_last_dim = 512
 
-        self.pre_fc1 = nn.Linear(resnet18_last_dim, 4096)
-        self.pre_fc2 = nn.Linear(4096, 4096)
-        nn.init.kaiming_normal_(self.pre_fc1.weight)
-        nn.init.kaiming_normal_(self.pre_fc2.weight)
-
-        lstm_dim = 512
-        num_layers = 2
-        resnet18_last_dim = 4096
-        if bidirectional:
-            self.gru = nn.GRU(resnet18_last_dim, int(lstm_dim / 2), bidirectional=True, num_layers=num_layers,
-                              batch_first=batch_first)
-        else:
-            self.gru = nn.GRU(resnet18_last_dim, lstm_dim, bidirectional=True, num_layers=num_layers,
-                              batch_first=batch_first)
-
-        self.fc = nn.Linear(lstm_dim, class_num)
-        nn.init.kaiming_normal_(self.fc.weight)
+        self.comb_fc1 = nn.Linear(resnet18_last_dim * 2, 512)
+        class_num = math.factorial(frame_num)
+        self.combination_list = list(itertools.combinations(list(range(frame_num)), 2))
+        self.comb_fc2 = nn.Linear(512 * len(self.combination_list), class_num)
+        nn.init.kaiming_normal_(self.comb_fc1.weight)
+        nn.init.kaiming_normal_(self.comb_fc2.weight)
 
     # xの形は(バッチサイズ, RNNへの入力数, チャンネル数, 解像度, 解像度)の5次元配列である必要がある
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-
-        # (バッチサイズ x RNNへの入力数, チャンネル数, 解像度, 解像度)の4次元配列に変換する
-        # x = x.view(batch_size * sequence_length, x.shape[2], x.shape[3], x.shape[4])
-        # x = self.resnet18(x)
-        # x = x.view(batch_size, sequence_length, -1)
-        # output_shape -> (batch_size, seq_len, data_size)
-
-        # resnet18_last_dim = 512
-        # fs = torch.zeros(batch_size, sequence_length, resnet18_last_dim).cuda()
-        # for i in range(batch_size):
-        #     cnn = self.resnet18(x[i])
-        #     cnn = torch.flatten(cnn, 1)
-        #     fs[i, :, :] = cnn
-        # x = fs
-
-        return self.forward_method(x)
-
-    def classification_forward(self, x: torch.Tensor) -> torch.Tensor:
         sequence_length = x.shape[1]
         x = x.permute(1, 0, 2, 3, 4)  # (batch_size, seq_len, img) -> (seq_len, batch_size, img)
-        # x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(sequence_length)])
-        x = torch.stack(
-            [self.pre_fc2(self.pre_fc1(torch.flatten(self.resnet18(x[i]), 1))) for i in range(sequence_length)])
-        # output_shape -> (seq_len, batch_size, data_size), lstm.batch_first -> Flase
-        # x = self.lstm(x)[0]
-        x = self.gru(x)[0]
-        x = self.fc(x)
-        return x
-
-    def sorting_forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size = x.shape[0]
-        # シーケンスでバッチ処理をする
-        x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(batch_size)])
-        # output_shape -> (batch_size, seq_len, data_size), lstm.batch_first -> True
-        # x = self.lstm(x)[0]
-        x = self.gru(x)[0]
-        x = self.fc(x)
+        x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(sequence_length)])
+        comb_feat_list = []
+        for comb in self.combination_list:
+            comb_feat_list.append(self.comb_fc1(torch.cat([x[comb[0]], x[comb[1]]], 1)))
+        x = self.comb_fc2(torch.cat(comb_feat_list, 1))
         return x
 
 
