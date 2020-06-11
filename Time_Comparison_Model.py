b932651c95a1779e08e9ccb323389454c5213809
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-12 06:24:33 +0900

    update

--- Time_Comparison_Model.py
+++ Time_Comparison_Model.py
@@ -1,30 +1,35 @@
 import torch
 from torch import nn
-from torchvision.models import resnet18
-from torchvision.models.resnet import BasicBlock
+# from torchvision.models import resnet18
+# from torchvision.models.resnet import BasicBlock
+from torchvision.models.alexnet import alexnet
 
 
 class TimeComparison(nn.Module):
     def __init__(self, bidirectional: bool = True, pretrained: bool = True):
         super().__init__()
 
-        resnet18_modules = [module for module in (resnet18(pretrained=pretrained).modules())][1:-1]
-        resnet18_modules_cut = resnet18_modules[0:4]
-        resnet18_modules_cut.extend(
-            [module for module in resnet18_modules if type(module) == nn.Sequential and type(module[0]) == BasicBlock])
-        resnet18_modules_cut.append(resnet18_modules[-1])
-        self.resnet18 = nn.Sequential(*resnet18_modules_cut)
-        resnet18_last_dim = 512
+        # resnet18_modules = [module for module in (resnet18(pretrained=pretrained).modules())][1:-1]
+        # resnet18_modules_cut = resnet18_modules[0:4]
+        # resnet18_modules_cut.extend(
+        #     [module for module in resnet18_modules if type(module) == nn.Sequential and type(module[0]) == BasicBlock])
+        # resnet18_modules_cut.append(resnet18_modules[-1])
+        # self.cnn = nn.Sequential(*resnet18_modules_cut)
+        # cnn_last_dim = 512
+
+        alex_model = alexnet(pretrained=pretrained)
+        self.cnn = nn.Sequential(*alex_model.features, alex_model.avgpool)
+        cnn_last_dim = 9216
 
         lstm_dim = 512
         batch_first = False
         if bidirectional:
             self.lstm_forward = self.bidirectional_forward
-            self.lstm = nn.LSTM(resnet18_last_dim, int(lstm_dim / 2), bidirectional=True, num_layers=2,
+            self.lstm = nn.LSTM(cnn_last_dim, int(lstm_dim / 2), bidirectional=True, num_layers=2,
                                 batch_first=batch_first)
         else:
             self.lstm_forward = self.normal_forward
-            self.lstm = nn.LSTM(resnet18_last_dim, lstm_dim, bidirectional=False, num_layers=2, batch_first=batch_first)
+            self.lstm = nn.LSTM(cnn_last_dim, lstm_dim, bidirectional=False, num_layers=2, batch_first=batch_first)
 
         # self.fc = nn.Linear(lstm_dim * 2, 2)
         # nn.init.kaiming_normal_(self.fc.weight)
@@ -54,9 +59,9 @@ class TimeComparison(nn.Module):
         seq_half = seq_len // 2
         x = x.permute(1, 0, 2, 3, 4)  # (batch_size, seq_len, img) -> (seq_len, batch_size, img)
 
-        x0 = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(0, seq_half)])
+        x0 = torch.stack([torch.flatten(self.cnn(x[i]), 1) for i in range(0, seq_half)])
         x0 = torch.mean(self.lstm(x0)[0], 0)
-        x1 = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(seq_half, seq_len)])
+        x1 = torch.stack([torch.flatten(self.cnn(x[i]), 1) for i in range(seq_half, seq_len)])
         x1 = torch.mean(self.lstm(x1)[0], 0)
         x = torch.cat([x0, x1], 1)
 
@@ -67,9 +72,9 @@ class TimeComparison(nn.Module):
         seq_half = seq_len // 2
         x = x.permute(1, 0, 2, 3, 4)  # (batch_size, seq_len, img) -> (seq_len, batch_size, img)
 
-        x0 = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(0, seq_half)])
+        x0 = torch.stack([torch.flatten(self.cnn(x[i]), 1) for i in range(0, seq_half)])
         x0 = self.lstm(x0)[0][-1]
-        x1 = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(seq_half, seq_len)])
+        x1 = torch.stack([torch.flatten(self.cnn(x[i]), 1) for i in range(seq_half, seq_len)])
         x1 = self.lstm(x1)[0][-1]
         x = torch.cat([x0, x1], 1)
 
