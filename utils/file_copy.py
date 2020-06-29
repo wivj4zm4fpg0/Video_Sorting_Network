38b172a6329cae5961a1206212ff48f5a922087c
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-29 14:25:01 +0900

    update

--- utils/file_copy.py
+++ utils/file_copy.py
@@ -12,6 +12,6 @@ for class_ in os.listdir(input_dir):
     class_path = os.path.join(input_dir, class_)
     for video in os.listdir(class_path):
         video_path = os.path.join(class_path, video)
-        command = f'cp {video_path} {output_dir}'
+        command = f'cp -r {video_path} {output_dir}'
         print(command)
         os.system(command)
