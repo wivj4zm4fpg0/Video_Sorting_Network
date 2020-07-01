0d4ac93cb063c99e0c78afa7b72fbc01072aa566
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-07-02 07:33:37 +0900

    update

--- utils/analysis_frame_num.py
+++ utils/analysis_frame_num.py
@@ -9,11 +9,15 @@ input_dir = args.input_dir
 frame_num = args.frame_num
 
 result = {}
+dataset_len = 0
+dataset_mean = 0
 for class_ in os.listdir(input_dir):
     class_path = os.path.join(input_dir, class_)
     for video in os.listdir(class_path):
         video_path = os.path.join(class_path, video)
         video_len = len(os.listdir(video_path))
+        dataset_len += 1
+        dataset_mean += video_len
         count = video_len // frame_num
         if count in result.keys():
             result[count] += 1
@@ -21,3 +25,5 @@ for class_ in os.listdir(input_dir):
             result[count] = 0
 
 print(f'{result = }')
+print(f'{dataset_len = }')
+print(f'mean = {dataset_mean / dataset_len}')
