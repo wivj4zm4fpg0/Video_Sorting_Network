45ea2367816b441e24a69aa6bfc43ed4d619e97d
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-07-02 11:50:39 +0900

    update

--- utils/generate_sort_label_cnn3d.py
+++ utils/generate_sort_label_cnn3d.py
@@ -1,56 +1,46 @@
 import argparse
-import itertools
 import os
-import random
 import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str, required=True)
-parser.add_argument('--frame_num', type=int, default=4, required=False)
-parser.add_argument('--cnn3d_frame_num', type=int, default=8, required=False)
-parser.add_argument('--output_txt', type=str, required=True)
+parser.add_argument('--cnn3d_frame_num', type=int, default=16, required=False)
+parser.add_argument('--output_csv', type=str, required=True)
 args = parser.parse_args()
 input_dir = args.input_dir
-frame_num = args.frame_num
+clip_num = 3
 cnn3d_frame_num = args.cnn3d_frame_num
-crop_video_len = cnn3d_frame_num * frame_num
-output_txt = args.output_txt
+crop_video_len = cnn3d_frame_num * clip_num
+output_csv = args.output_csv
+sort_len = 3
+labels = [[[0, 1, 2], [0, 1, 2], 0], [[0, 2, 1], [0, 2, 1], 1], [[1, 0, 2], [1, 0, 2], 2],
+          [[2, 1, 0], [0, 1, 2], 0], [[1, 2, 0], [0, 2, 1], 1], [[2, 0, 1], [1, 0, 2], 2]]
 
-sort_seq = list(itertools.permutations(list(range(frame_num)), frame_num))
-shuffle_list = []
-for v in sort_seq:
-    v = list(v)
-    if v[::-1] in shuffle_list:
-        continue
-    shuffle_list.append(v)
-shuffle_len = len(shuffle_list)
 
-column = 'name'
-for i in range(frame_num):
-    column += f',label{i + 1}'
-with open(output_txt, mode='w') as f:
-    f.write(f'{column},order\n')
+def l2s(x: list) -> str:
+    return re.sub(r', ', '_', str(x)[1:-1])
 
-count = 0
-for class_ in os.listdir(input_dir):
-    class_path = os.path.join(input_dir, class_)
-    for video in os.listdir(class_path):
-        print(f'{video = }')
-        video_frame_num = len(os.listdir(os.path.join(class_path, video)))
-        if crop_video_len > video_frame_num:
-            count += 1
-            continue
-        start_index = random.randint(0, video_frame_num - crop_video_len)
-        frame_indices = list(range(video_frame_num))[start_index:start_index + crop_video_len:cnn3d_frame_num]
-        frame_indices = [list(range(frame_index, frame_index + cnn3d_frame_num + 1)) for frame_index in frame_indices]
-        label = shuffle_list[random.randint(0, shuffle_len - 1)]
-        shuffle_label = list(range(frame_num))
-        for i, v in enumerate(label):
-            shuffle_label[i] = [frame_indices[v][0], frame_indices[v][-1]]
-        label_str = ''
-        for v in shuffle_label:
-            label_str += f'{re.sub(", ", "_", str(v)[1:-1])},'
-        order = re.sub(', ', '_', str(label)[1:-1])
-        with open(output_txt, mode='a') as f:
-            f.write(f'{video},{label_str[:-1]},{order}\n')
-print(f'{count = }')
+
+with open(output_csv, mode='w') as f:
+    f.write('file_name,frames1,frames2,frames3,label1,label2\n')
+
+    for class_ in os.listdir(input_dir):
+        class_path = os.path.join(input_dir, class_)
+        for video in os.listdir(class_path):
+            video_path = os.path.join(class_path, video)
+            video_len = len(os.listdir(video_path))
+            subtracted_frame = video_len - crop_video_len
+            interval_frame = subtracted_frame // (clip_num + 1)
+            frame_indices = list(range(video_len))
+            extracted_frames = frame_indices[interval_frame:interval_frame * clip_num + crop_video_len * (
+                    clip_num - 1) + 1:interval_frame + crop_video_len]
+            extracted_frame_indices = [list(range(v, v + crop_video_len)) for v in extracted_frames]
+            sort_list = [extracted_frame_indices[i][cnn3d_frame_num * j:cnn3d_frame_num * (j + 1)] for j in
+                         range(sort_len) for i in range(clip_num)]
+            for clips in sort_list:
+                for order in labels:
+                    label_list = []
+                    for v in order[0]:
+                        label_list.append(clips[v])
+                    f.write(f'{class_}/{video},{l2s(label_list[0])},{l2s(label_list[1])},'
+                            + f'{l2s(label_list[2])},{l2s(order[1])},{order[2]}\n')
